f5210b5f2468a2640b6ef35948877c9406b4b256
Melvin Mbae <melvinmuthomi95@gmail.com>
2023-07-17 17:24:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function saturdayFun(def = "roller-skate") {
+    return `This Saturday, I want to ${def}!`
+
+
+}
+saturdayFun()
+
+function mondayWork(def = "go to the office") {
+    return `This Monday, I will ${def}.`
+
+
+}
+mondayWork()
+
+function wrapAdjective(string = "*") {
+    const innerFunction = function inner(def = "special") {
+        return `You are ${string}${def}${string}!`;
+    };
+    return innerFunction;
+}
+
+wrapAdjective("*")("a dedicated programmer");
